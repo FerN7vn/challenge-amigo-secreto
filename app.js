ccb73cdd8eb33f3bdaa54673220431f13b4b0a8c
FerN7vn <fermlsterminator@gmail.com>
2025-01-29 23:21:09 -0300

    Tentando criar uma função para adicionar os amigos

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let listaDeAmigos = [ ];
+let numeroDeAmigos = 0;
 
+function exibirNomesNaTela(tag, texto) {
+    
+}
 
+function adicionarAmigo () {
+    let nomeDoAmigo = document.querySelector('input').value;
+    
+    if (nomeDoAmigo == "") {
+        alert("Por favor insira um nome válido"); 
+    } else {
+        listaDeAmigos.push(nomeDoAmigo);
+        numeroDeAmigos++;
+    }
+}
     
