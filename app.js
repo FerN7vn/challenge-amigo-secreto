42f225b535c5baa6d84d365a591175f35f17af5a
FerN7vn <fermlsterminator@gmail.com>
2025-02-01 09:34:24 -0300

    Quase pronto!

--- app.js
+++ app.js
@@ -2,26 +2,43 @@
 let listaDeAmigos = [ ];
 let numeroDeAmigos = 0;
 let amigoSorteado; 
+/* let listaAmigos = document.getElementById('listaAmigos');
+let amigo = document.createElement('li');
+let strongTag = document.createElement('strong'); */
 
 /* function exibirNomesNaTela() {
+    for (let i = 0; i < listaDeAmigos.length; i++) {
+        strongTag.textContent = listaDeAmigos[i];
+        amigo.appendChild(strongTag);
+        listaAmigos.appendChild(amigo);
+    }
+}  */
+
+function exibirNomesNaTela() {
     let listaAmigos = document.getElementById('listaAmigos');
-    const amigo = document.createElement('li');
+    listaAmigos.innerHTML = "";
+
+    let conteudo = '';
 
     for (let i = 0; i < listaDeAmigos.length; i++) {
-        listaAmigos.textContent = listaAmigos[i];
+        conteudo += "<li><strong>" + listaDeAmigos[i] + "</strong></li>";
     }
-} */
+
+    listaAmigos.innerHTML = conteudo;
+}
  
 function sortearAmigo() {
     amigoSorteado = parseInt(Math.random() * numeroDeAmigos + 1);
     console.log(listaDeAmigos[amigoSorteado -1]);
+    listaAmigos.innerHTML = "";
     
     resultadoDoSorteio();
+    /* reiniciarJogo(); */
 }
 
 function resultadoDoSorteio() {
     let resultado = document.getElementById('resultado');
-    resultado.innerHTML = "<li>O amigo secreto é: " + amigoSorteado + "<li>"
+    resultado.innerHTML = "<li>O amigo secreto é: " + listaDeAmigos[amigoSorteado -1] + "<li>"
 }
 
 function adicionarAmigo () {
@@ -37,10 +54,20 @@ function adicionarAmigo () {
         console.log(listaDeAmigos[numeroDeAmigos - 1]);
         limparCampo();
     }
+    
+    exibirNomesNaTela();
 }
 
 function limparCampo() {
     let campo = document.querySelector('input');
     campo.value = "";
 }
+
+function reiniciarJogo() {
+    if (listaDeAmigos.length > 0) {
+    amigoSorteado = ""; 
+    listaDeAmigos = [];
+    resultado.innerHTML = "";
+    }
+} 
     
